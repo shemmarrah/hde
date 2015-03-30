fd254ed9c47ff72efc5b533511399f73bc52d57b
shemmarrah john <shemmarah@hdmade.com>
2015-03-30 15:01:31 -0400

    yugio card module has been made.

---
+++ pokemon.card.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<link rel="stylesheet" href="css/reset.css">
+		<link rel="stylesheet" type="text/css" href="css/game-card.css">
+	</head>
+	<body>
+		<div class="game-card">
+			<div class="game-card--display">
+				<img class="game-card--image" src="css/images/typhlosion.png" alt="typhlosion.jpg">
+			</div>
+			<div class="game-card--details">
+				<h4 class="game-card--title">Typhlosion</h4>
+				<p> Quilava, named Exbo, evolved into Typhlosion to protect Silver, Crystal, and himself from Lugia. Afterwards, its larger size allowed it to be a method of faster travel.</p>
+			</div>
+		</div>
+	</body>	
+</html>
---
+++ yugio.card.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>blue eyes white dragon</title>
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/yugio-card.css">
+</head>
+<body>
+	<div class="yugio-container">
+		<div class="yugio-card--display">
+			<div class="yugio-card--element"></div>
+			<img class="yugio-card--image" src="css/images/winged-dragon.jpg" alt="winged-dragon">	
+		</div>
+		<div class="yugio-card--description">
+			<h4 class="yugio-card--name">Winged dragon of ra</h4>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, perferendis nostrum nisi sint autem explicabo, provident dolore commodi veritatis, beatae consectetur harum ut quia maxime tenetur cumque tempora non itaque?</p>
+		</div>
+	</div>
+</body>
+</html>
